65c64abb2adc1dbfde7aaaf958fbe4ca9c25d482
Hussen Fakhrulddin <hf556482@gmail.com>
2016-05-01 23:21:57 -0700

    init

---
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+            public class PaymentService
+        {
+            private static object _SyncRoot;
+            private static PaymentService _Instance;
+
+            private PaymentService()
+            { }
+
+            public static PaymentService GetInstance()
+            {
+                if (_Instance == null)
+                {
+                    // obtain lock so no other threads can access it until the current thread is done
+                    lock (_SyncRoot)
+                    {
+                        // is it still null? another thread may have initialized _Instance before
+                        // the current thread obtained the lock
+                        if (_Instance == null)
+                        {
+                            _Instance = new PaymentService();
+                        }
+                    }
+                }
+
+                return _Instance;
+            }
+        }
+    }
+}
